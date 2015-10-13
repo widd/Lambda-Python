abfe929bde2952a7171e2b5ef47007c289b3c8ff
marcusant <mstojcevich@gmail.com>
2015-10-13 00:24:32 -0400

    Add legacy upload API. Untested.

--- lmda/views/upload.py
+++ lmda/views/upload.py
@@ -25,6 +25,11 @@ class ResponseEncoder(json.JSONEncoder):
         return values
 
 
+class LegacyResponseFile:
+    def __init__(self, url):
+        self.url = url
+
+
 @app.route('/upload')
 def upload():
     return render_template('upload.html')
@@ -42,6 +47,101 @@ def restrictions():
     response.no_extension_types = app.config['NO_EXTENSION_TYPES']
 
     return Response(json.dumps(response, cls=ResponseEncoder), mimetype='application/json')
+
+
+@app.route('/upload', methods=['POST'])
+def legacy_upload():  # LEGACY, DO NOT USE, WILL BE REMOVED SOON
+    api_key = request.form.get('apikey')
+
+    response = UploadResponse()
+
+    if api_key is None:
+        response.errors.append('No api key POSTed')
+
+        # No 400 code is intentional because the old API gave a 200
+        # Not setting json mime type is intentional because the old API didn't set it
+        return json.dumps(response, cls=ResponseEncoder)
+
+    from lmda.models import User
+    user = User.by_api_key(api_key)
+
+    if user is None:
+        response.errors.append('Invalid API key')
+        # No 400 code is intentional because the old API gave a 200
+        # Not setting json mime type is intentional because the old API didn't set it
+        return json.dumps(response, cls=ResponseEncoder)
+
+    file = request.files['file']
+    if file:
+        extension_split = file.filename.split('.')
+        if len(extension_split) < 1:
+            response.errors.append('File is missing extension. Name was ' + file.filename + '.')
+            return json.dumps(response, cls=ResponseEncoder), 400
+
+        extension = extension_split[-1]
+        extension_allowed = app.config.get('ALLOWED_TYPES', None) is None or extension in app.config['ALLOWED_TYPES']
+        if not extension_allowed:
+            response.errors.append('Extension not allowed: ' + extension)
+
+        filename = gen_filename()
+        if filename is None:
+            response.errors.append('Error generating filename')
+            return json.dumps(response, cls=ResponseEncoder), 500
+
+        try:
+            relative_filename = os.path.join(app.config['UPLOAD_FOLDER'], filename + '.' + extension)
+            absolute_filename = os.path.abspath(relative_filename)
+            file.save(relative_filename)
+
+            response.files = [LegacyResponseFile(filename)]
+
+            if extension not in app.config['NO_EXTENSION_TYPES']:
+                response.files[0].url += '.' + extension
+
+            if file.content_length > app.config['MAX_FILESIZE_MB']*1000000:
+                response.errors.append('Filesize ' + str(file.content_length/1000000) + ' > ' + app.config['MAX_FILESIZE_MB'] + ' MB')
+                # No 400 code is intentional because the old API gave a 200
+                # Not setting json mime type is intentional because the old API didn't set it
+                return json.dumps(response, cls=ResponseEncoder)
+
+            # Make sure they didn't lie in content_length
+            file.seek(0, os.SEEK_END)
+            file_length = file.tell()
+            if file_length > app.config['MAX_FILESIZE_MB']*1000000:
+                response.errors.append('Filesize ' + str(file_length/1000000) + ' > ' + app.config['MAX_FILESIZE_MB'] + ' MB')
+                # No 400 code is intentional because the old API gave a 200
+                # Not setting json mime type is intentional because the old API didn't set it
+                return json.dumps(response, cls=ResponseEncoder)
+
+            from lmda.models import File
+            file = File(owner=user.id, name=filename, extension=extension, encrypted=False, local_name=file.filename,
+                        has_thumbnail=False)
+            db.session.add(file)
+            db.session.commit()
+
+            # SUCCESS !!!
+
+            # Create thumbnail
+            if not current_user.is_anonymous and extension in app.config['THUMBNAIL_TYPES']:
+                from lmda.models import Thumbnail
+                thumbnail_process_pool.apply_async(
+                    create_thumbnail,
+                    args=(absolute_filename, filename),
+                    callback=add_thumbnail
+                )
+
+            return json.dumps(response, cls=ResponseEncoder)
+        except IOError as e:
+            sys.stderr.write(str(e))
+            sys.stderr.write('\n')
+            response.errors.append('Error saving file')
+            response.errors.append(str(e))
+            # No 500 code is intentional because the old API gave a 200
+            return json.dumps(response, cls=ResponseEncoder)
+    else:
+        response.errors.append('No file sent. Please send a file as "file".')
+        # Not setting json mime type is intentional because the old API didn't set it
+        return json.dumps(response, cls=ResponseEncoder)
 
 
 @app.route('/api/upload', methods=['PUT'])
