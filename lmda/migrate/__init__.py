4d631bc50be5da495f41581833adf178a1651350
Marcus Stojcevich <mstojcevich@gmail.com>
2015-10-13 13:50:26 -0400

    Create migration script

---
+++ lmda/migrate/__init__.py
@@ -0,0 +1,25 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from lmda import db
+from lmda.migrate import oldmodels
+from lmda.models import User, File, Paste
+
+db.create_all()
+
+engine = create_engine('mysql://lambda:lambda@localhost/lambda_go')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+for user in session.query(oldmodels.User).all():
+    new_user = User(id=user.id, username=user.username, password=user.password, creation_date=user.creation_date, api_key=user.apikey, encryption_enabled=user.encryption_enabled, theme_name=user.theme_name)
+    db.session.add(new_user)
+
+for file in session.query(oldmodels.File).all():
+    new_file = File(id=file.id, owner=file.owner, name=file.name, extension=file.extension, encrypted=file.encrypted, local_name=file.local_name, upload_date=file.upload_date, has_thumbnail=False)
+    db.session.add(new_file)
+
+for paste in session.query(oldmodels.Paste).all():
+    new_paste = Paste(id=paste.id, owner=paste.owner, name=paste.name, content_json=paste.content_json, is_code=paste.is_code, upload_date=paste.upload_date)
+    db.session.add(new_paste)
+
+db.session.commit()
---
+++ lmda/migrate/oldmodels.py
@@ -0,0 +1,39 @@
+from sqlalchemy import Column, Integer, String, Date, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
+
+
+def User(Base):
+    __tablename__ = 'users'
+
+    id = Column(Integer, primary_key=True)
+    username = Column(String)
+    password = Column(String)
+    creation_date = Column(Date)
+    apikey = Column(String)
+    encryption_enabled = Column(Boolean)
+    theme_name = Column(String)
+
+
+def File(Base):
+    __tablename__ = 'files'
+
+    id = Column(Integer, primary_key=True)
+    owner = Column(Integer)
+    name = Column(String)
+    extension = Column(String)
+    upload_date = Column(DateTime)
+    encrypted = Column(Boolean)
+    local_name = Column(String)
+
+
+def Paste(Base):
+    __tablename__ = 'pastes'
+
+    id = Column(Integer, primary_key=True)
+    owner = Column(Integer)
+    name = Column(String)
+    upload_date = Column(DateTime)
+    content_json = Column(String)
+    is_code = Column(Boolean)
