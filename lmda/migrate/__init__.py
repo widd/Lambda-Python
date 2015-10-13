760f8bbeaa10ea58f858a650fbe9fa4db52ec534
Marcus Stojcevich <mstojcevich@gmail.com>
2015-10-13 19:56:57 -0400

    Fix migrate

--- lmda/migrate/__init__.py
+++ lmda/migrate/__init__.py
@@ -13,13 +13,16 @@ session = Session()
 for user in session.query(oldmodels.User).all():
     new_user = User(id=user.id, username=user.username, password=user.password, creation_date=user.creation_date, api_key=user.apikey, encryption_enabled=user.encryption_enabled, theme_name=user.theme_name)
     db.session.add(new_user)
+    db.session.commit()
 
 for file in session.query(oldmodels.File).all():
     new_file = File(id=file.id, owner=file.owner, name=file.name, extension=file.extension, encrypted=file.encrypted, local_name=file.local_name, upload_date=file.upload_date, has_thumbnail=False)
     db.session.add(new_file)
+    db.session.commit()
 
 for paste in session.query(oldmodels.Paste).all():
     new_paste = Paste(id=paste.id, owner=paste.owner, name=paste.name, content_json=paste.content_json, is_code=paste.is_code, upload_date=paste.upload_date)
     db.session.add(new_paste)
+    db.session.commit()
 
-db.session.commit()
+exit(0)
