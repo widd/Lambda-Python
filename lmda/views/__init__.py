c8c95badf6a7ea49ed9e6505359deac62c4161d4
widd <widd@dad.town>
2016-10-04 22:20:03 -0700

    Have create_static_html make shtml dir automatically

--- lmda/views/__init__.py
+++ lmda/views/__init__.py
@@ -4,8 +4,13 @@ from lmda.views import auth, misc, paste, upload, file, admin
 
 
 def create_static_html():
+    shtml_dir = 'lmda/static/shtml'
+    if not os.path.exists(shtml_dir):
+        os.makedirs(shtml_dir)
+
     for filename in os.listdir('lmda/templates'):
         print("Compiling %s" % filename)
         compiled = render_template(filename)
-        with open('lmda/static/shtml/%s' % filename, 'w') as outfile:
+        filepath = 'lmda/static/shtml/%s' % filename
+        with open(filepath, 'w') as outfile:
             outfile.write(compiled.encode('utf-8'))
